30d16dbcd34069039bc30d45c7f6a71d169b3e19
Priscilla H <priscillamhuynh@gmail.com>
2018-07-11 08:57:40 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,8 @@
 class Person
-  attr_accessor :name, :eye_color, :handed
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+
 
   def initialize(name:,eye_color:,handed:)
     @name=name
