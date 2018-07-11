84ddca228fecfc9b990b5ac400c4455d0216d160
Priscilla H <priscillamhuynh@gmail.com>
2018-07-11 08:56:48 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
+  attr_accessor :name, :eye_color, :handed 
+
+  def initialize(name:,eye_color:,handed:)
+    @name=name 
+    @eye_color=eye_color
+    @handed=handed
 end
